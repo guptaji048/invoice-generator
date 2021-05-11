c811682bec7090135ec8528319ecd9b1312856cb
Rahul Gupta <guptaji048@gmail.com>
2021-05-11 14:02:26 +0530

    State and city data added

---
+++ src/components/Data.js
@@ -0,0 +1,5 @@
+export const State = ['Maharashtra', 'Gujarat', 'Haryana', 'Uttar Pradesh'];
+export const Maharashtra = ['Mumbai', 'Pune', 'Nagpur', 'Nashik', 'Thane'];
+export const Gujarat = ['Ahmedabad', 'Gandhinagar', 'Rajkot', 'Surat', 'Vadodara'];
+export const Haryana = ['Ambala', 'Faridabad', 'Gurgaon', 'Panipat', 'Rohtak'];
+export const UttarPradesh = ['Kanpur', 'Lucknow', 'Gorakhpur', 'Noida', 'Agra'];
