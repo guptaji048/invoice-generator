4583c9c7ce7445989b0dfad8089fbdb7c0936ca6
Rahul Gupta <guptaji048@gmail.com>
2021-05-11 19:17:51 +0530

    Submit button added
    - Styling updated

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Typography, Divider, makeStyles, Grid, TextField, Checkbox, FormControlLabel } from '@material-ui/core';
+import { Container, Typography, Divider, makeStyles, Grid, TextField, Checkbox, FormControlLabel, Button } from '@material-ui/core';
 import ReceiverDetails from './forms/ReceiverDetails';
 import BillingDetails from './forms/BillingDetails';
 import ProductDetails from './ProductDetails';
@@ -15,6 +15,7 @@ function InvoiceForm() {
   const [invoiceData, setInvoiceData] = useState({
     invoiceDate: '',
     dueDate: '',
+    grandTotal: 0,
   });
   const [receiverData, setReceiverData] = useState({
     name: '',
@@ -33,6 +34,7 @@ function InvoiceForm() {
     name: '',
     quantity: 0,
     baseAmount: 0,
+    totalAmount: 0,
   }]);
   const [billingFlag, setBillingFlag] = useState(false);
 
@@ -66,60 +68,63 @@ function InvoiceForm() {
   }, [billingFlag])
 
   return (
-    <Container maxWidth="md" className={classes.container}>
-      <Typography variant="h4">Add Invoice Details</Typography>
-      <Divider />
-      <Grid container spacing={2} style={{ marginTop: 5 }}>
-        <Grid item md={6}>
-          <TextField
-            label="Invoice Date"
-            type="date"
-            variant="outlined"
-            value={invoiceData.invoiceDate}
-            onChange={date => handleDateChange(date.target.value)}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            margin="dense"
-            fullWidth
-          />
-        </Grid>
+    <div style={{ margin: 10, padding: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Container maxWidth="md" className={classes.container}>
+        <Typography variant="h4">Add Invoice Details</Typography>
+        <Divider />
+        <Grid container spacing={2} style={{ marginTop: 5 }}>
+          <Grid item md={6}>
+            <TextField
+              label="Invoice Date"
+              type="date"
+              variant="outlined"
+              value={invoiceData.invoiceDate}
+              onChange={date => handleDateChange(date.target.value)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="dense"
+              fullWidth
+            />
+          </Grid>
 
-        <Grid item md={6}>
-          <TextField
-            label="Due Date"
-            type="date"
-            variant="outlined"
-            value={invoiceData.dueDate}
-            onChange={date => setInvoiceData({ ...invoiceData, dueDate: date.target.value })}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            margin="dense"
-            fullWidth
-          />
+          <Grid item md={6}>
+            <TextField
+              label="Due Date"
+              type="date"
+              variant="outlined"
+              value={invoiceData.dueDate}
+              onChange={date => setInvoiceData({ ...invoiceData, dueDate: date.target.value })}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="dense"
+              fullWidth
+            />
+          </Grid>
+          <Grid item md={12}>
+            <ReceiverDetails receiverData={receiverData} setReceiverData={setReceiverData} />
+            <FormControlLabel
+              control={
+                <Checkbox
+                  checked={billingFlag}
+                  onChange={() => setBillingFlag(!billingFlag)}
+                  color="primary"
+                />
+              }
+              label="Click if billing address same as receiver address"
+            />
+          </Grid>
+          <Grid item md={12}>
+            <BillingDetails billingData={billingData} setBillingData={setBillingData} />
+          </Grid>
+          <Grid item md={12}>
+            <ProductDetails productData={productData} setProductData={setProductData} setInvoiceData={setInvoiceData} invoiceData={invoiceData} />
+          </Grid>
         </Grid>
-        <Grid item md={12}>
-          <ReceiverDetails receiverData={receiverData} setReceiverData={setReceiverData} />
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={billingFlag}
-                onChange={() => setBillingFlag(!billingFlag)}
-                color="primary"
-              />
-            }
-            label="Click if billing address same as receiver address"
-          />
-        </Grid>
-        <Grid item md={12}>
-          <BillingDetails billingData={billingData} setBillingData={setBillingData} />
-        </Grid>
-        <Grid item md={12}>
-          <ProductDetails productData={productData} setProductData={setProductData} />
-        </Grid>
-      </Grid>
-    </Container>
+      </Container>
+      <Button variant="contained" style={{ width: '25%', marginTop: 10, backgroundColor: '#0082d6', color: 'white' }}>Submit</Button>
+    </div>
   )
 }
 
