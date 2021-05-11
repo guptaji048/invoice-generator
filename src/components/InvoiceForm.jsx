9d2d76675f76f6a52252dd88fdb8b392e2ad0216
Rahul Gupta <guptaji048@gmail.com>
2021-05-11 16:28:42 +0530

    Invoice Component updated
    - Date function added
    - Separate component for Receiver and Billing added

--- src/components/InvoiceForm.jsx
+++ src/components/InvoiceForm.jsx
@@ -1,104 +1,113 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Typography, Divider, makeStyles, Grid, TextField, Checkbox, FormControlLabel } from '@material-ui/core';
-import {
-  MuiPickersUtilsProvider,
-  KeyboardDatePicker,
-} from '@material-ui/pickers';
-import MomentUtils from '@date-io/moment';
-import DropDown from './utils/DropDown';
-import { State, Maharashtra } from './Data';
+import ReceiverDetails from './forms/ReceiverDetails';
+import BillingDetails from './forms/BillingDetails';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    height: '100vh',
     border: '1px solid #e0e0e0',
   }
 }));
 
 function InvoiceForm() {
   const classes = useStyles();
+  const [invoiceData, setInvoiceData] = useState({
+    invoiceDate: '',
+    dueDate: '',
+  });
+  const [receiverData, setReceiverData] = useState({
+    name: '',
+    address: '',
+    state: '',
+    city: '',
+    pincode: '',
+  });
+  const [billingData, setBillingData] = useState({
+    address: '',
+    state: '',
+    city: '',
+    pincode: '',
+  });
+  const [billingFlag, setBillingFlag] = useState(false);
+
+  const handleDateChange = (date) => {
+    var dueDate = new Date(date);
+    dueDate.setDate(dueDate.getDate() + 30);
+    setInvoiceData({ ...invoiceData, invoiceDate: date, dueDate: dueDate.toISOString().split('T')[0] })
+    console.log(invoiceData);
+  }
+
+  const handleBillingCheckbox = () => {
+    if (billingFlag === true) {
+      setBillingData({
+        address: receiverData.address,
+        state: receiverData.state,
+        city: receiverData.city,
+        pincode: receiverData.pincode,
+      });
+    } else {
+      setBillingData({
+        address: '',
+        state: '',
+        city: '',
+        pincode: '',
+      });
+    }
+  }
+
+  useEffect(() => {
+    handleBillingCheckbox();
+  }, [billingFlag])
 
   return (
     <Container maxWidth="md" className={classes.container}>
       <Typography variant="h4">Add Invoice Details</Typography>
       <Divider />
-      <Grid container spacing={2}>
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <Grid item md={6}>
-            <KeyboardDatePicker
-              variant="inline"
-              inputVariant="outlined"
-              label="Invoice Date"
-              format="MM/dd/yyyy"
-              minDate={new Date()}
-              //value={selectedDate}
-              // onChange={date => handleDateChange(date)}
-              margin="dense"
-              fullWidth
-            />
-          </Grid>
-          <Grid item md={6}>
-            <KeyboardDatePicker
-              variant="inline"
-              inputVariant="outlined"
-              label="Due Date"
-              format="MM/dd/yyyy"
-              //value={selectedDate}
-              // onChange={date => handleDateChange(date)}
-              margin="dense"
-              fullWidth
-            />
-          </Grid>
-        </MuiPickersUtilsProvider>
+      <Grid container spacing={2} style={{ marginTop: 5 }}>
         <Grid item md={6}>
-          <Typography variant="h6">Receiver Details</Typography>
-          <TextField label="Raised To" variant="outlined" margin="dense" fullWidth style={{ paddingBottom: 10 }} />
-          <TextField label="Address" variant="outlined" margin="dense" multiline rows="4" fullWidth style={{ paddingBottom: 10 }} />
-          <DropDown
-            name="State"
-            label="State"
-            // value={opType}
-            options={State}
-          // onChange={handleInputChange}
+          <TextField
+            label="Invoice Date"
+            type="date"
+            variant="outlined"
+            value={invoiceData.invoiceDate}
+            onChange={date => handleDateChange(date.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            margin="dense"
+            fullWidth
           />
-          <DropDown
-            name="City"
-            label="City"
-            // value={opType}
-            options={Maharashtra}
-          // onChange={handleInputChange}
+        </Grid>
+
+        <Grid item md={6}>
+          <TextField
+            label="Due Date"
+            type="date"
+            variant="outlined"
+            value={invoiceData.dueDate}
+            onChange={date => setInvoiceData({ ...invoiceData, dueDate: date.target.value })}
+            InputLabelProps={{
+              shrink: true,
+            }}
+            margin="dense"
+            fullWidth
           />
-          <TextField label="Pin Code" variant="outlined" margin="dense" fullWidth style={{ paddingBottom: 10 }} />
+        </Grid>
+        <Grid item md={12}>
+          <ReceiverDetails receiverData={receiverData} setReceiverData={setReceiverData} />
           <FormControlLabel
             control={
               <Checkbox
-                // checked={state.checkedB}
-                // onChange={handleChange}
-                name="checkedB"
+                checked={billingFlag}
+                onChange={() => setBillingFlag(!billingFlag)}
                 color="primary"
               />
             }
             label="Click if billing address same as receiver address"
           />
         </Grid>
-        <Grid item md={6}>
-          <Typography variant="h6">Billing Address</Typography>
-          <TextField label="Address" variant="outlined" margin="dense" multiline rows="4" fullWidth style={{ paddingBottom: 10 }} />
-          <DropDown
-            name="State"
-            label="State"
-            // value={opType}
-            options={State}
-          // onChange={handleInputChange}
-          />
-          <DropDown
-            name="City"
-            label="City"
-            // value={opType}
-            options={Maharashtra}
-          // onChange={handleInputChange}
-          />
-          <TextField label="Pin Code" variant="outlined" margin="dense" fullWidth style={{ paddingBottom: 10 }} />
+        <Grid item md={12}>
+          <BillingDetails billingData={billingData} setBillingData={setBillingData} />
         </Grid>
       </Grid>
     </Container>
